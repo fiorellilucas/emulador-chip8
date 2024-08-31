9b566f8cd4e2097068635639ec3840aaec9a0e41
Lucas <fiorellilucas@gmail.com>
2024-08-31 14:52:36 -0300

    Implementar opcode 0x1NNN

--- main.cpp
+++ main.cpp
@@ -3,23 +3,47 @@
 #include <filesystem>
 
 uint16_t memory[4096] = { 0 };
+uint16_t pc = 0x200;
+uint16_t v[16] = { 0 };
+uint16_t opcode;
+uint16_t opcode_data;
 
 int main() {
     using namespace std;
 
-    filesystem::path game_path("C:/Users/lucas/Documents/chip8-roms/games/Pong.ch8");
+    filesystem::path game_path("C:/Users/lucas/Documents/chip8-roms/games/Cave.ch8");
     ifstream file(game_path, ios::binary);
+
+    uint16_t game_size = filesystem::file_size(game_path);
 
     if (file.is_open()) {
         cout << "File opened successfully.\n";
-        
-        uint16_t game_size = filesystem::file_size(game_path);
 
         for (uint16_t i = 0; i < game_size; i++) {
             file.read(reinterpret_cast<char*>(&memory[0x200 + i]), 1);
             //cout << hex << showbase << memory[0x200 + i] << "\n";
         }
 
+    }
+    
+    while (true) {
+        opcode = (memory[pc] << 8 | memory[pc + 1]);
+        bool increment_pc = true;
+
+        switch (opcode & 0xF000) {
+            case 0x1000:
+                opcode_data = (opcode & 0x0FFF);
+                pc = opcode_data;
+                increment_pc = false;
+                break;
+            default:
+                cout << "opcode " << hex << showbase << opcode << " unknown\n";
+                break;
+        }
+        
+        if (increment_pc) {
+            pc += 2;
+        }
     }
     
     return 0;
