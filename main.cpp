1470c0bf26d72e1b4e4e69889a8f323a1646602a
Lucas <fiorellilucas@gmail.com>
2024-08-31 12:00:50 -0300

    Carregar jogo Pong na memória virtual

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <fstream>
+#include <filesystem>
+
+uint16_t memory[4096] = { 0 };
 
 int main() {
-    std::cout << "Hello world";
+    using namespace std;
+
+    filesystem::path game_path("C:/Users/lucas/Documents/chip8-roms/games/Pong.ch8");
+    ifstream file(game_path, ios::binary);
+
+    if (file.is_open()) {
+        cout << "File opened successfully.\n";
+        
+        uint16_t game_size = filesystem::file_size(game_path);
+
+        for (uint16_t i = 0; i < game_size; i++) {
+            file.read(reinterpret_cast<char*>(&memory[0x200 + i]), 1);
+            //cout << hex << showbase << memory[0x200 + i] << "\n";
+        }
+
+    }
     
     return 0;
 }
