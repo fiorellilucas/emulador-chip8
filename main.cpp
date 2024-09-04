353d15d432b1fde893236f9d381e5eb68d7e541f
Lucas <fiorellilucas@gmail.com>
2024-09-04 18:30:50 -0300

    Make loading the game a separate function

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include "main.h"
 #include "chip8.h"
 
-int main() {
-    std::filesystem::path game_path("C:/Users/lucas/Documents/chip8-roms/games/Pong.ch8");
-    std::ifstream file(game_path, std::ios::binary);
+void load_game(Chip8& chip8);
 
     Chip8 chip8;
+    load_game(chip8);
 
     if (file.is_open()) {
         std::cout << "File opened successfully.\n";
@@ -25,4 +24,18 @@ int main() {
     }
 
     return 0;
+}
+
+void load_game(Chip8& chip8) {
+    std::filesystem::path game_path("C:/Users/lucas/Documents/chip8-roms/games/Pong.ch8");
+    std::ifstream file(game_path, std::ios::binary);
+
+    if (file.is_open()) {
+        std::cout << "File opened successfully.\n";
+        uintmax_t game_size = std::filesystem::file_size(game_path);
+
+        for (uint16_t i = 0; i < game_size; i++) {
+            file.read(reinterpret_cast<char*>(&chip8.memory[chip8.pc + i]), 1);
+        }
+    }
 }
